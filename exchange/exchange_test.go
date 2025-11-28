170f22893bd06f7eb311599debee8225ce710b85
banky <bankiadebajo1@gmail.com>
2025-11-28 02:19:41 -0500

    Temporarily disable exchange test

--- exchange/exchange_test.go
+++ exchange/exchange_test.go
@@ -1,42 +1,33 @@
 package exchange
 
-import (
-	"context"
-	"fmt"
-	"testing"
+// func TestOrder(t *testing.T) {
+// 	privateKey, err := crypto.GenerateKey()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	"github.com/banky/go-hyperliquid/constants"
-	"github.com/ethereum/go-ethereum/crypto"
-)
+// 	e, err := New(Config{
+// 		BaseURL: constants.TESTNET_API_URL,
+// 		// BaseURL:    constants.MAINNET_API_URL,
+// 		SkipWS:     true,
+// 		PrivateKey: privateKey,
+// 	})
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-func TestOrder(t *testing.T) {
-	privateKey, err := crypto.GenerateKey()
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	r, err := e.Order(
+// 		context.Background(),
+// 		"BTC",
+// 		true,
+// 		10,
+// 		10,
+// 		OrderType{Limit: &LimitOrder{Tif: "Ioc"}},
+// 	)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	e, err := New(Config{
-		BaseURL: constants.TESTNET_API_URL,
-		// BaseURL:    constants.MAINNET_API_URL,
-		SkipWS:     true,
-		PrivateKey: privateKey,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	fmt.Println(r)
 
-	r, err := e.Order(
-		context.Background(),
-		"BTC",
-		true,
-		10,
-		10,
-		OrderType{Limit: &LimitOrder{Tif: "Ioc"}},
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	fmt.Println(r)
-
-}
+// }
